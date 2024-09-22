3a476fb0e773796b5f9e7318ced86958b0862d6a
stanislav <stanislav.vasilchenko@yandex.ru>
2024-09-22 13:35:45 +0300

    add get all prod url

--- routers/product.py
+++ routers/product.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from shemas.product import ProductOut, ProductCreate
@@ -10,3 +12,8 @@ router = APIRouter(prefix="/product", tags=["product"])
 @router.post('/', response_model=ProductOut, summary="Create a product")
 async def get_products(product: ProductCreate, db: Session = Depends(get_db)):
     return await product_crud.create_product(db, product)
+
+
+@router.get('/', response_model=List[ProductOut], summary="Get all products")
+async def get_products(db: Session = Depends(get_db)):
+    return await product_crud.get_all_products(db)
