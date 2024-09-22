d0a221b0b3388d194f856ceb817da14fd2fe8584
stanislav <stanislav.vasilchenko@yandex.ru>
2024-09-22 13:29:40 +0300

    add product create url

---
+++ routers/product.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from shemas.product import ProductOut, ProductCreate
+from configs.db_config import get_db
+from crud import product as product_crud
+
+router = APIRouter(prefix="/product", tags=["product"])
+
+
+@router.post('/', response_model=ProductOut, summary="Create a product")
+async def get_products(product: ProductCreate, db: Session = Depends(get_db)):
+    return await product_crud.create_product(db, product)
