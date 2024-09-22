d3d3e17d1a4ddf54983302c1fd427fecaf20b232
stanislav <stanislav.vasilchenko@yandex.ru>
2024-09-22 14:07:04 +0300

    add prod update url

--- routers/product.py
+++ routers/product.py
@@ -2,7 +2,7 @@ from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from shemas.product import ProductOut, ProductCreate
+from shemas.product import ProductOut, ProductCreate, ProductUpdate
 from configs.db_config import get_db
 from crud import product as product_crud
 
@@ -23,5 +23,13 @@ async def get_products(db: Session = Depends(get_db)):
 async def get_product(product_id: int, db: Session = Depends(get_db)):
     db_product = await product_crud.get_product_by_id(db, product_id)
     if db_product is None:
-        raise HTTPException(status_code=404, detail="Product not found")
+        raise HTTPException(status_code=404, detail=f"Product with id {product_id} not found")
     return db_product
+
+
+@router.put('/{id}', response_model=ProductOut, summary="Update a product by id")
+async def update_product(product_id: int, product: ProductUpdate, db: Session = Depends(get_db)):
+    prod_db = await product_crud.product_update(db, product, product_id)
+    if prod_db is None:
+        raise HTTPException(status_code=404, detail=f"Product with id {product_id} not found")
+    return prod_db
