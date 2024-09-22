e6bd8be7b0ba32f3ee018a5519ae10ca3a92cdd0
stanislav <stanislav.vasilchenko@yandex.ru>
2024-09-22 12:46:00 +0300

    changed col id, price

--- models/product.py
+++ models/product.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, Float
 
 from configs.db_config import Base
 
 
 class Product(Base):
     __tablename__ = 'product'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, index=True)
     description = Column(Text)
-    price = Column(Integer, index=True)
+    price = Column(Float, index=True)
     quantity = Column(Integer, index=True)
