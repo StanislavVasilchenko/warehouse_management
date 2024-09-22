2b668c39655c878fa02ad216e6a4decb52b68f2c
stanislav <stanislav.vasilchenko@yandex.ru>
2024-09-22 13:42:17 +0300

    add get one prod by id url

--- routers/product.py
+++ routers/product.py
@@ -1,12 +1,12 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from shemas.product import ProductOut, ProductCreate
 from configs.db_config import get_db
 from crud import product as product_crud
 
-router = APIRouter(prefix="/product", tags=["product"])
+router = APIRouter(prefix="/products", tags=["product"])
 
 
 @router.post('/', response_model=ProductOut, summary="Create a product")
@@ -17,3 +17,11 @@ async def get_products(product: ProductCreate, db: Session = Depends(get_db)):
 @router.get('/', response_model=List[ProductOut], summary="Get all products")
 async def get_products(db: Session = Depends(get_db)):
     return await product_crud.get_all_products(db)
+
+
+@router.get('/{id}', response_model=ProductOut, summary="Get a product by id")
+async def get_product(product_id: int, db: Session = Depends(get_db)):
+    db_product = await product_crud.get_product_by_id(db, product_id)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return db_product
