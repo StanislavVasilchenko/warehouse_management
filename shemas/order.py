0243e0d83a2f641a36cd516407a55fca71ca4521
stanislav <stanislav.vasilchenko@yandex.ru>
2024-09-23 10:30:48 +0300

    create schemas by Order

---
+++ shemas/order.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel
+from models.order import OrderSatus
+
+
+class OrderBase(BaseModel):
+    status: OrderSatus = OrderSatus.in_process
+
+
+class OrderItemsBase(BaseModel):
+    order_id: int
+    product_id: int
+    product_quantity: int
+
+
+class OrderItemCreate(OrderItemsBase):
+    order_id: Optional[int] = None
+    product_id: int
+    product_quantity: int
+
+
+class OrderItemOut(OrderItemsBase):
+    id: int
+    order_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class OrderOut(OrderBase):
+    date_creation: datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    id: int
+    order_items: List[OrderItemOut]
+
+    class Config:
+        orm_mode = True
+
+
+class OrderUpdate(OrderBase):
+    status: OrderSatus
